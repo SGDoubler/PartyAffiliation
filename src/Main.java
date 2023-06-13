0ef84450123999f6cdcdb034c9d0f7fb859398a5
SGDoubler <doublesn@mail.uc.edu>
2023-06-12 21:51:08 -0400

    task3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.Scanner;
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        String partyChoice = "";
+        String party = "R - Republican\tD - Democrat\tI - Independent\tO -Other";
+
+        System.out.println("What is your party affiliation?");
+        System.out.println(party);
+
+        partyChoice = in.nextLine();
+
+        if (partyChoice.equalsIgnoreCase("D"))
+        {
+            System.out.println("You get a Democratic Donkey");
+        }
+        else if (partyChoice.equalsIgnoreCase("R"))
+        {
+            System.out.println("You get a Republican Elephant");
+        }
+        else if (partyChoice.equalsIgnoreCase("I"))
+        {
+            System.out.println("You get an Independent Person");
+        }
+        else
+        {
+            System.out.println("You are the wisest of us all!");
+        }
+
     }
 }
